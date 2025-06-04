8ad7e2fe0c88c7b931665a2e18a3a542f2299694
heavengabi <heavenxg89@gmail.com>
2025-06-04 16:59:58 -0300

    Add files via upload

---
+++ UC2-Gabriela Silva da Rosa/Aula2.js
@@ -0,0 +1,34 @@
+//Operadores Aritmeticos
+
+
+
+//Soma
+const valor1 = 54
+const valor2 = 10
+const resultado = valor1 + valor2
+
+    console.log(resultado)
+
+//Subtração
+const valor3 = 100
+const valor4 = 50
+const resultado4 = valor3 - valor4
+    console.log(resultado4)
+
+//Multiplicação
+const valor5 = 55
+const valor6 = 5
+const resultado2 = valor5 * valor6
+    console.log (resultado2)
+
+//Divisão
+const valor7 = 60
+const valor8= 30
+const resultado3 = valor7/valor8
+    console.log(resultado3)
+
+//Resto
+const valor9 = 11
+const valor10 = 4
+const resultado5 = valor9 % valor10
+    console.log(resultado5)
---
+++ UC2-Gabriela Silva da Rosa/ExercicioOperadores.js
@@ -0,0 +1,22 @@
+
+//Exercicio operadores aritmeticos
+
+const numeroInicial = 100
+const multiplicador = 10
+const divisor = 5
+
+const resultado1 = numeroInicial * multiplicador
+    console.log("Multiplicando o número inicial:", resultado1)
+
+const resultado2 = resultado1 / divisor 
+    console.log("Dividindo o resultado pelo divisor:", resultado2)
+
+const resultado3 = resultado2 % 7
+    console.log ("O resto da divisão por 7 é:", resultado3)
+
+const resultado4 = resultado3 + 10
+    console.log("A soma do resultado + 10 é: ", resultado4)
+
+const resultado5 = resultado4 - (3*numeroInicial)
+    console.log("A subtração do número inicial pelo resultado vezes 3 é:", resultado5)
+
