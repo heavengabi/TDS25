1f61b870b066fec837afff779c115c9a521c2d51
heavengabi <heavenxg89@gmail.com>
2025-06-04 17:01:17 -0300

    Add files via upload

---
+++ UC2-Gabriela Silva da Rosa/Exercicio2.js
@@ -0,0 +1,25 @@
+//exercicio 1//
+const nome1 = "Bidu"
+let idade = 3
+let falar = true
+
+    console.log ("Meu cachorro", nome1, "tem", idade , "anos.", "Ele pode falar?", true)
+
+//exercicio 2//
+const sabor = "calabresa"
+let valor = 66
+let pizzaVoa = true
+
+    console.log ("Pizza de", sabor, "por R$", valor, "Ela voa?", true)
+
+
+//exercicio 3//
+const nome = "Wall-le"
+let danca = 8
+const estaDancando = true
+
+    console.log ("O", nome, "já aprendeu", danca, "Passos!", "Ele está dançando agora?", true)
+
+
+console.log(typeof nome)
+console.log(typeof estaDancando)
