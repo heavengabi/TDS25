87f116c303142a962dc77165456fda93b361dcb9
heavengabi <heavenxg89@gmail.com>
2025-06-11 17:09:48 -0300

    Delete UC2-Gabriela Silva da Rosa directory

--- UC2-Gabriela Silva da Rosa/Exercicio1.js
+++
@@ -1,9 +0,0 @@
-
-
-
-const nome = "Gabriela"
-const sobrenome = "Silva da Rosa"
-let idade = 17
-let estudante = true
-
-console.log("Olá!", "Meu nome é", nome, sobrenome, "Tenho", idade, "anos.", "Sou estudante", true )
